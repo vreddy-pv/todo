f17db129e794537a68e9aadef53b6eeed6351f74
vvred <vvred@example.com>
2026-02-28 11:02:43 +0530

    chore: Move todo-list to independent project
    - Separated Angular todo-list module from backend service
    - Todo-list is now an independent project in parent directory
    - Backend service no longer has frontend dependencies
    - Frontend and backend can be developed and deployed separately

--- todo-service/todo-list/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { TodoListComponent } from './todo-list/todo-list.component';
-
-export const routes: Routes = [
-  { path: '', component: TodoListComponent },
-  { path: 'finance', loadChildren: () => import('./finance/finance.module').then(m => m.FinanceModule) },
-  { path: '**', redirectTo: '' }
-];
